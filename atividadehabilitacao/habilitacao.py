054a0bde5919268b652dd412f6b984b6dd0d416f
mightbehappyy <97134972+mightbehappyy@users.noreply.github.com>
2022-09-15 16:36:57 -0300

    Create habilitacao.py

---
+++ atividadehabilitacao/habilitacao.py
@@ -0,0 +1,16 @@
+from random import randint
+rodas = int(input("Quantas rodas o veiculo tem?"))
+peso = int(input("Qual é o peso do veículo"))
+pessoas = int(input("Quantas pessoas esse veículo acomoda?"))
+
+if rodas == 2 or rodas == 3:
+    print("Habilitação tipo A")
+if rodas == 4 and pessoas <= 8 and peso <= 3500:
+    print("Habilitação tipo B")
+if rodas >= 4 :
+    if 3500 <= peso <= 6000:
+        print("Habilitação tipo C")
+    elif pessoas > 8:
+        print("Habilitação tipo D")
+    elif peso > 8000:
+        print("Habilitação tipo E")
