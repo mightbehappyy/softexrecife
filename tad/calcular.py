edbb45291f96ca469ace4e940e981923c5514651
mightbehappyy <97134972+mightbehappyy@users.noreply.github.com>
2022-11-16 12:45:23 -0300

    Create calcular.py

---
+++ tad/calcular.py
@@ -0,0 +1,22 @@
+class calculadora:
+    som = ''
+    mult = ''
+    sub = ''
+    div = ''
+
+
+
+
+    def calccomplexos():
+        #nao consegui fazer
+        return 0
+    def calcularbasicos():
+        calc = calculadora
+        calc.som = int(input('Digite um numero')) + int(input('Digite outro numero'))
+        calc.mult = int(input('Digite um numero')) * int(input('Digite outro numero'))
+        calc.sub = int(input('Digite um numero')) - int(input('Digite outro numero'))
+        calc.div = int(input('Digite um numero')) / int(input('Digite outro numero'))
+        return calc
+    def printar():
+        #nao consegui fazer
+        return 0
