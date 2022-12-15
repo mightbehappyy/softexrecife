72d29fa75b3a051e626d7f04d982c51ad8d9bcbe
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2022-12-15 16:01:03 -0300

    Create calculadora.js

---
+++ javascript/calculadora.js
@@ -0,0 +1,16 @@
+var num1 = prompt("Digite um numero ")
+var num2 = prompt("Digite outro numero ")
+var operador = prompt("Digite o operador")
+if (operador == "+"){
+    println(num1 + num2)
+}
+if (operador == "-") {
+    println(num1 + num2)
+}
+if (operador == "*"){
+    println(num1 * num2)
+}
+if (operador == "/"){
+    println(num1 / num2)
+    println("A sobra foi: ", num1 % num2)
+}
