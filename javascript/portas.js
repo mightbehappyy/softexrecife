de6074a0b6eb85ac467d5d1606ee466aa83b0746
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2022-12-18 16:49:37 -0300

    Update portas.js

--- javascript/portas.js
+++ javascript/portas.js
@@ -1,1 +1,15 @@
-/// Em construcao
+const express =  require('express')
+
+const app = express();
+
+app.get('/',(req, res) =>{
+    res.send('<h1>Nosso primeiro servidor<h1>')
+})
+
+app.get('/json', (req,res)=>{
+    res.json({Tittle:"Nosso primeiro server", online: true})
+})
+
+app.listen(3000, () => {
+    console.log("Servidor iniciado!")
+})
