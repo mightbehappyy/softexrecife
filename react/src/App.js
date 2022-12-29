03e9cfb24ca1747d0726e16861f69b6a579cbc61
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2022-12-29 00:22:31 -0300

    Update App.js

--- react/src/App.js
+++ react/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import HelloWorld from './componentes/HelloWorld';
 
 function App() {
   return (
     <div className="App">
-      <h1>Pedro Luiz</h1>
+      <h1>Bom dia</h1>
+      <HelloWorld/>
     </div>
   );
 }
