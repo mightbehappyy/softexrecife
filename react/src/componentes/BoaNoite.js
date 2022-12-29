44324666f225d2ca89296474174a7f6d3bf09ba0
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2022-12-29 00:30:25 -0300

    Add files via upload

---
+++ react/src/componentes/BoaNoite.js
@@ -0,0 +1,13 @@
+function BoaNoite(props){
+    return (
+        <div>
+            <p>
+                Boa noite {props.nome}
+            </p>
+
+        </div>
+
+    )
+}
+
+export default BoaNoite
