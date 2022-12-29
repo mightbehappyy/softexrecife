03f65679cd941bd6772a0582e33c0a99f810aaf5
Pedro <97134972+mightbehappyy@users.noreply.github.com>
2022-12-29 00:30:59 -0300

    Update App.js

--- react/src/App.js
+++ react/src/App.js
@@ -1,12 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import HelloWorld from './componentes/HelloWorld';
+import BoaNoite from './componentes/BoaNoite';
 
 function App() {
   return (
     <div className="App">
       <h1>Bom dia</h1>
       <HelloWorld/>
+      <BoaNoite nome="Pedro"/>
+      <BoaNoite nome="Joao"/>
+      <BoaNoite nome="Gabriel"/>
     </div>
   );
 }
